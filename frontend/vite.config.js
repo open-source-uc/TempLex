5544521524c0586e9d6235c04aa0274f61611018
Wh4rp <matias.fernandez@uc.cl>
2022-12-15 12:37:22 -0300

    added path aliases

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '@components', replacement: resolve(__dirname, 'src', 'components') },
+      { find: '@services', replacement: resolve(__dirname, 'src', 'services') },
+      { find: '@store', replacement: resolve(__dirname, 'src', 'store') },
+      { find: '@utils', replacement: resolve(__dirname, 'src', 'utils') },
+      { find: '@views', replacement: resolve(__dirname, 'src', 'views') },
+    ],
+  }
 })
